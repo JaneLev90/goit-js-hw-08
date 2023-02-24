8e4a111af9973a67f332a7d46f48f57d7634bd0c
JaneLev90 <jlev888@gmail.com>
2023-02-24 19:36:45 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.ready().then(() =>{
+    const savedTime = localStorage.getItem('lastPlayedTime');
+    const initialTime = savedTime ? parseFloat(savedTime) : 0;
+    player.setCurrentTime(initialTime);
+});
+
+window.addEventListener('beforeunload', () => {
+    player.getCurrentTime().then((currentTime) => {
+localStorage.setItem('lastPlayedTime', currentTime.toString());
+    });
+});
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+// let lastPlayedTime = 0;
+// iframe.addEventListener('timeupdate', () =>{
+//     lastPlayedTime = iframe.currentTime;
+// })
+// document.addEventListener('beforeunload', () => {
+//     localStorage.setItem('lastPlayedTime', lastPlayedTime);
+// })
+// document.addEventListener('load', () => {
+//     const savedTime = localStorage.getItem('lastPlayedTime');
+//     if (savedTime) {
+//         iframe.currentTime = savedTime;
+//     }
+// })
